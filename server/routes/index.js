13501604cc46ca9c34ff875ad683eaa6cc226266
Alyssa Ngo <amaengo@protonmail.com>
2024-08-05 17:37:59 +1000

    Added packages needed to connect to database

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET all personality test results */ 
+router.get("/results", (req, res) => {
+  const results = [
+    { title: "Front-End Developer", description: "Focuses on the user interface and user experience of web applications. They work with HTML, CSS, JavaScript, and frameworks like React or Angular." },
+    { title: "Back-End Developer", description: "" },
+    { title: "Full Stack Developer", description: "" },
+  ];
+
+  res.json(results);
+});
+
 module.exports = router;
